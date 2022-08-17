9f2e99d266b100cd0ac8f7130e836506c440e768
AdixonCamacho <mcleud.leznark@gmail.com>
2022-08-17 17:11:51 -0500

    3D ANIMATION ADD

--- Javascript/sectionSkills.js
+++ Javascript/sectionSkills.js
@@ -1,16 +1,20 @@
-const showText = document.querySelectorAll(".text__skills")
+const showText = document.querySelectorAll(".div__icon")
 const iconRotate = document.querySelectorAll(".icon-circle-down")
 
-console.log(iconRotate);
+// console.log(iconRotate);
 
 const showTextArr = [...showText]
 
 const windowView = document.querySelectorAll(".text__animation")
 
-showTextArr.map((text , index) => text.addEventListener('click', ()=> {
-     windowView[index].classList.toggle("active-details")
-     iconRotate[index].classList.toggle("iconRotate")
-}))
+showTextArr.map((text , index) =>{
+     text.addEventListener('click', ()=> {
+          windowView.forEach(el => el.classList.remove("active-details"))
+          iconRotate.forEach(el => el.classList.remove('iconRotate'))
+          windowView[index].classList.add("active-details")
+          iconRotate[index].classList.add("iconRotate")
+     })
+})
 
 
 
